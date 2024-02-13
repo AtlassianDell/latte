8e0a4b83e3a7ec90f88b5f275590ebb306747666
AtlassianDell <129398355+AtlassianDell@users.noreply.github.com>
2024-02-13 12:07:25 -0500

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,1 +1,14 @@
+import sys
 
+filename = sys.argv[1]
+filecont = open(filename).split("\n")
+
+class token:
+  class symb:
+    define = "::"
+    associate = ":"
+    add = "+"
+    sub = "-"
+    mul = "*"
+    div = "/"
+  
